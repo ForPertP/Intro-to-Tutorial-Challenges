3e3d17c35513cea1fcb289ac5ca5648478818720
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-11-10 02:00:56 +0900

    Update Intro-to-Tutorial-Challenges.cpp

--- Intro-to-Tutorial-Challenges.cpp
+++ Intro-to-Tutorial-Challenges.cpp
@@ -1,31 +1,11 @@
 int introTutorial(int V, vector<int> arr)
 {
-    int i = 0;
-    int n = arr.size() - 1;
-
-    while (i <= n)
-    {
-        int m = (i + n) / 2;
-        
-        if (arr.at(m) == V)
-        {
-            return m;
-        }
-        else if (arr.at(m) > V)
-        {
-            n = m - 1;
-        }
-        else
-        {
-            i = m + 1;
-        }
-    }
-
-    return 0;
+    auto index = std::find(arr.begin(), arr.end(), V);
+    return index - arr.begin();
 }
 
 
-int introTutorial2(int V, vector<int> arr)
+int introTutorial(int V, vector<int> arr)
 {
     int left = 0;
     int right = arr.size();
@@ -53,24 +33,3 @@ int introTutorial2(int V, vector<int> arr)
     return index;
 }
 
-/*
-return Arrays.binarySearch(arr,V);
-
-    int left = 0;
-    int right = arr.length;
-    int median,index=0;
-    while(left<=right)
-    {
-        median = (left+right)/2;
-        if(arr[median]==V)
-        {
-            index=median;
-            break;
-        }
-        else if(arr[median]<V)
-            left = median + 1;
-        else
-            right = median - 1;
-    }
-    return index;
-*/
