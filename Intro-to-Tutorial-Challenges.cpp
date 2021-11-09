466bfb51d6e7f015263bc42825a153ecd377bfd6
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-11-09 15:01:15 +0900

    Update Intro-to-Tutorial-Challenges.cpp

--- Intro-to-Tutorial-Challenges.cpp
+++ Intro-to-Tutorial-Challenges.cpp
@@ -25,6 +25,34 @@ int introTutorial(int V, vector<int> arr)
 }
 
 
+int introTutorial2(int V, vector<int> arr)
+{
+    int left = 0;
+    int right = arr.size();
+    int median = 0;
+    int index = 0;
+    
+    while (left <= right)
+    {
+        median = (left + right) / 2;
+        if (arr[median] == V)
+        {
+            index = median;
+            break;
+        }
+        else if (arr[median] < V)
+        {
+            left = median + 1;
+        }
+        else
+        {
+            right = median - 1;
+        }
+    }
+    
+    return index;
+}
+
 /*
 return Arrays.binarySearch(arr,V);
 
