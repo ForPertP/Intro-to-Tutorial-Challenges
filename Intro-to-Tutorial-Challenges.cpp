f5bf969336dd5f8bb82a08327b54a0720a4099dd
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-06 04:14:26 +0900

    Update Intro-to-Tutorial-Challenges.cpp

--- Intro-to-Tutorial-Challenges.cpp
+++ Intro-to-Tutorial-Challenges.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'introTutorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER V
+ *  2. INTEGER_ARRAY arr
+ */
+
 int introTutorial(int V, vector<int> arr)
 {
     auto index = std::find(arr.begin(), arr.end(), V);
