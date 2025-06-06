deb8906c72f5987f23cf1bf76088a3e147601c53
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-07 04:25:42 +0900

    Update Intro-to-Tutorial-Challenges.cpp

--- Intro-to-Tutorial-Challenges.cpp
+++ Intro-to-Tutorial-Challenges.cpp
@@ -15,38 +15,38 @@ vector<string> split(const string &);
  *  2. INTEGER_ARRAY arr
  */
 
-int introTutorial(int V, vector<int> arr)
+int introTutorial(int V, std::vector<int> arr)
+{
+    auto it = std::lower_bound(arr.begin(), arr.end(), V);
+    if (it != arr.end() && *it == V)
+        return it - arr.begin();
+    else
+        return -1;
+}
+
+
+int introTutorial2(int V, vector<int> arr)
+{
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right)
+    {
+        int median = (left + right) / 2;
+        if (arr[median] == V)
+            return median;
+        else if (arr[median] < V)
+            left = median + 1;
+        else
+            right = median - 1;
+    }
+
+    return -1;
+}
+
+
+int introTutorial3(int V, vector<int> arr)
 {
     auto index = std::find(arr.begin(), arr.end(), V);
     return index - arr.begin();
 }
-
-
-int introTutorial(int V, vector<int> arr)
-{
-    int left = 0;
-    int right = arr.size() - 1;
-    int median = 0;
-    int index = 0;
-    
-    while (left <= right)
-    {
-        median = (left + right) / 2;
-        if (arr[median] == V)
-        {
-            index = median;
-            break;
-        }
-        else if (arr[median] < V)
-        {
-            left = median + 1;
-        }
-        else
-        {
-            right = median - 1;
-        }
-    }
-    
-    return index;
-}
-
