b63ccf016515bf5ca70a68ad8e7180300150e5a0
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-09 05:12:15 +0900

    Update Intro-to-Tutorial-Challenges.cpp

--- Intro-to-Tutorial-Challenges.cpp
+++ Intro-to-Tutorial-Challenges.cpp
@@ -118,4 +118,9 @@ int main()
     return 0;
 }
 
-
+string ltrim(const string &str)
+{
+    string s(str);
+    s.erase( s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); }));
+    return s;
+}
