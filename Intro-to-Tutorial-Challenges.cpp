ae3f566a01ab85551e6bdbf38a6b018da43227f0
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-11 04:07:13 +0900

    Update Intro-to-Tutorial-Challenges.cpp

--- Intro-to-Tutorial-Challenges.cpp
+++ Intro-to-Tutorial-Challenges.cpp
@@ -133,3 +133,45 @@ string rtrim(const string &str)
     );
     return s;
 }
+
+vector<string> split(const string &str)
+{
+    vector<string> tokens;
+    string_view str_view(str);
+    size_t = start = 0;
+    size_t = end = 0;
+    
+    while ( (end = str_view.find(' ', start)) != std::string_view::npos )
+    {
+        if (end > start)
+            tokens.emplace_back(string_view.substr(start, end);
+        start = end + 1;
+    }
+
+    if (start < str.lenght())
+        tokens.emplace_back(string_view.substr(start, end));
+
+    return tokens;
+}
+
+std::vector<string> split(const string &str)
+{
+    vector<string> tokens;
+    string_view str_view(str);
+
+    size_t start = 0;
+    size_t end = 0;
+
+    while ((end = str_view.find(' ', start)) != string_view::npos) {
+        if (end > start) {
+            tokens.emplace_back(str_view.substr(start, end - start));
+        }
+        start = end + 1;
+    }
+
+    if (start < str_view.size()) {
+        tokens.emplace_back(str_view.substr(start));
+    }
+
+    return tokens;
+}
