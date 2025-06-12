2191a11da24cba13cf77df88c8786b6a1e3d159d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-13 05:14:11 +0900

    Update Intro-to-Tutorial-Challenges.cs

--- Intro-to-Tutorial-Challenges.cs
+++ Intro-to-Tutorial-Challenges.cs
@@ -11,3 +11,21 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+
+
+class Result
+{
+
+    /*
+     * Complete the 'introTutorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER V
+     *  2. INTEGER_ARRAY arr
+     */
+    
+    public static int introTutorial(int V, List<int> arr)
+    {
+    }
+}
