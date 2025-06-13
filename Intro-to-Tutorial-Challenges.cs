9f7d4a41cbb3c16d15b39da8afa8af13a6b8529f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-14 06:13:10 +0900

    Update Intro-to-Tutorial-Challenges.cs

--- Intro-to-Tutorial-Challenges.cs
+++ Intro-to-Tutorial-Challenges.cs
@@ -29,3 +29,26 @@ class Result
     {
     }
 }
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int V = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.introTutorial(V, arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
