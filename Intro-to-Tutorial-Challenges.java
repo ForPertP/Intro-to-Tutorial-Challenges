f61bd603cf913b7b3310fdc094c8ebf68e27a115
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-19 06:00:05 +0900

    Update Intro-to-Tutorial-Challenges.java

--- Intro-to-Tutorial-Challenges.java
+++ Intro-to-Tutorial-Challenges.java
@@ -23,3 +23,27 @@ class Result {
      */
   
 }
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int V = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.introTutorial(V, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
